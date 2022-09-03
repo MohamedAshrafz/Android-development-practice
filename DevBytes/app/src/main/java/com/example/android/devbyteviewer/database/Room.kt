e721058c5d25371cbf6b40ea620e3d8d9c00da1d
Mohamed Ashraf <mohamed.ashraf.z879@gmail.com>
2022-09-04 01:41:22 +0200

    update after adding the DevBytes app

---
+++ DevBytes/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.devbyteviewer.database
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface VideoDao {
+    @Query("SELECT * FROM databasevideo")
+    fun getVideos(): LiveData<List<DatabaseVideo>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(vararg videos: DatabaseVideo)
+}
+
+@Database(entities = [DatabaseVideo::class], version = 1)
+abstract class VideoDatabase : RoomDatabase() {
+
+    abstract val VideoDatabase: VideoDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: VideoDatabase? = null
+
+        // singleton pattern
+        // only one instance of the database is allowed to exist
+        // if there is no database make a one, if there is exist a database return it
+        fun getInstance(context: Context): VideoDatabase {
+            synchronized(this) {
+                var instance = INSTANCE
+
+                if (instance == null) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        VideoDatabase::class.java,
+                        "videos_database"
+                    )
+                        .build()
+
+                    INSTANCE = instance
+                }
+                // returning the instance not INSTANCE to benefit from the smart casting
+                // smart casting only works with local vars not class vars
+                return instance
+            }
+        }
+    }
+}
+
---
+++ RealEstateInMars/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.network
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MarsProperty(
+    @Json(name = "id")
+    val id: String,
+    @Json(name = "price")
+    val price: Double,
+    @Json(name = "type")
+    val type: String,
+    @Json(name = "img_src")
+    val imgSrcUrl: String
+
+
+) : Parcelable {
+    val isRental: Boolean
+        get() = type == "rent"
+}
+
+//{
+//    constructor(parcel: Parcel) : this(
+//        parcel.readString(),
+//        parcel.readDouble(),
+//        parcel.readString(),
+//        parcel.readString()
+//    ) {
+//    }
+//
+//    override fun writeToParcel(parcel: Parcel, flags: Int) {
+//        parcel.writeString(id)
+//        parcel.writeDouble(price)
+//        parcel.writeString(type)
+//        parcel.writeString(imgSrcUrl)
+//    }
+//
+//    override fun describeContents(): Int {
+//        return 0
+//    }
+//
+//    companion object CREATOR : Parcelable.Creator<MarsProperty> {
+//        override fun createFromParcel(parcel: Parcel): MarsProperty {
+//            return MarsProperty(parcel)
+//        }
+//
+//        override fun newArray(size: Int): Array<MarsProperty?> {
+//            return arrayOfNulls(size)
+//        }
+//    }
+//}
